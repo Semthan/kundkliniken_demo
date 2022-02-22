4cd3b312b6aaf4f96ba92df3c733ceb2545fcde4
sarmad Al-Bidhawi <sarmed.91@hotmail.com>
2022-02-22 18:47:09 +0100

    dropdown toggle

--- script.js
+++ script.js
@@ -3,16 +3,20 @@ document.addEventListener("DOMContentLoaded", function () {
   let togBtn = document.getElementById('toggle_btn')
   let hamBtn = document.getElementById('ham_btn')
   let closeBtn = document.getElementById('close_btn')
+  let mainDropNav = document.getElementById('main_drop_nav')
 
   let closed = true
+
   togBtn.addEventListener('click', function () {
     if (closed) {
       hamBtn.classList.add('hidden')
       closeBtn.classList.remove('hidden')
+      mainDropNav.classList.remove('hidden')
       closed = false
     } else {
       hamBtn.classList.remove('hidden')
       closeBtn.classList.add('hidden')
+      mainDropNav.classList.add('hidden')
       closed = true
     }
   })
